5ae113eb584ad2cb70e7a486e1ef7a050841ce7a
NikeDev01 <alishertoxtaboyev7@gmail.com>
2025-05-28 23:04:12 +0500

    Update intro.sql

--- Lesson-1/homework/intro.sql
+++ Lesson-1/homework/intro.sql
@@ -1,1 +1,60 @@
 
+
+
+---1. Define the following terms:
+
+---Data: Raw facts and figures without context. Example: 23, "John", true.
+
+--Database: A structured collection of data stored electronically for easy access, management, and update.
+
+--Relational Database: A type of database that stores data in tables related by keys.
+
+--Table: A collection of related data entries arranged in rows and columns.
+
+
+
+--2. Five key features of SQL Server:
+
+--1 High Availability (Always On Availability Groups)
+
+--2 Security (Encryption, Authentication, Roles)
+
+-- 3Business Intelligence tools (SSIS, SSRS, SSAS)
+
+-- 4Transaction Management (ACID compliance)
+
+ --5 Data Tools Integration (with Visual Studio and SSMS)
+
+
+ ---  3. Authentication modes in SQL Server:
+--Windows Authentication
+
+-- SQL Server Authentication
+
+CREATE DATABASE lesson_1;
+
+USE lesson_1;
+
+CREATE TABLE Students(
+
+StudentID INT PRIMARY Key,
+Name VARCHAR(50),
+Age INT 
+);
+
+INSERT INTO Students (StudentID, Name, Age)
+VALUES
+(1, 'Ali', 20),
+(2, 'Malika', 22),
+(3, 'Sherzod', 21);
+
+
+
+USE master;
+
+RESTORE DATABASE AdventureWorksDW2022
+FROM DISK = 'C:\Backups\AdventureWorksDW2022.bak'
+WITH 
+    MOVE 'AdventureWorksDW2022_Data' TO 'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\AdventureWorksDW2022.mdf',
+    MOVE 'AdventureWorksDW2022_Log' TO 'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\AdventureWorksDW2022_log.ldf',
+    REPLACE;
